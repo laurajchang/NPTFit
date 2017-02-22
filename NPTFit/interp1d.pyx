a6ca1ccb6d9609b03845ffd020062d0754cdc9be
Laura Chang <laurachang@nat-oitwireless-inside-vapornet100-d-16931.Princeton.EDU>
2017-02-22 17:49:59 -0500

    Added interp1d code

--- NPTFit/interp1d.pyx
+++ NPTFit/interp1d.pyx
@@ -60,3 +60,48 @@ def interp1d(np.ndarray[DTYPEf_t, ndim=1] x, np.ndarray[DTYPEf_t, ndim=1] y,
                  new_y[i] = (y[j] - y[j-1]) / (x[j] - x[j-1]) * (new_x[i] - x[j-1]) + y[j-1]
                  break
     return new_y
+
+def loginterp1d(np.ndarray[DTYPEf_t, ndim=1] x, np.ndarray[DTYPEf_t, ndim=1] y,
+               np.ndarray[DTYPEf_t, ndim=1] new_x):
+    """
+    interp1d(x, y, new_x)
+
+    Performs log-linear interpolation in 1d.
+
+    Parameters
+    ----------
+    x : 1-D ndarray (double type)
+        Array containg the x (abcissa) values. Must be monotonically
+        increasing.
+    y : 1-D ndarray (double type)
+        Array containing the y values to interpolate. Must have same length as x.
+    x_new: 1-D ndarray (double type)
+        Array with new abcissas to interpolate.
+
+    Returns
+    -------
+    new_y : 1-D ndarray
+        Interpolated values.
+    """
+    cdef int nx = x.shape[0]
+    cdef int ny = y.shape[0]
+    cdef int nx_new = new_x.shape[0]
+
+    assert (nx == ny), \
+        "x and y must have equal length!"
+    cdef int i, j
+    cdef np.ndarray[DTYPEf_t, ndim=1] new_y = np.zeros(nx_new, dtype=DTYPEf)
+
+    for i in range(nx_new):
+        if x[0] >= new_x[i]:
+            new_y[i] = y[0]
+            print("Certain values in new_x too small!")
+        elif x[nx-1] <= new_x[i]:
+            new_y[i] = y[nx-1]
+            print("Certain values in new_x too large!")
+        else:
+            for j in range(1, nx):
+                if x[j] > new_x[i]:
+                 new_y[i] = (y[j] - y[j-1]) / (np.log10(x[j]) - np.log10(x[j-1])) * (np.log10(new_x[i]) - np.log10(x[j-1])) + y[j-1]
+                 break
+    return new_y
